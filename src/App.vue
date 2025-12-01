a79597af5471007906dc97f1fff1b8098094bf55
Russias <russiasesteban@gmail.com>
2025-12-01 15:42:21 +0100

    first push

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div class="p-6 bg-blue-600 text-white rounded-xl text-xl">
+    Tailwind 4 fonctionne ! 🎉
+  </div>
+</template>
+
+
+<style scoped></style>
